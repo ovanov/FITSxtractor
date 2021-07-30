22551ef5fd042f31b1264e022fdcd19489ff3102
Jovanovic  Luka <ljovanovic@ethz.ch>
2021-07-30 13:32:20 +0200

    added setup files and readme

--- setup.py
+++ setup.py
@@ -5,23 +5,25 @@ setup(
   version = '0.1',
   license='MIT',
   description = 'This package extracts xml metadata from FITS output to a csv or xlsx file',
-  author = 'ovanov',                   # Type in your name
-  author_email = 'ovanov@protonmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  author = 'ovanov',
+  author_email = 'ovanov@protonmail.com',
+  url = 'https://github.com/ovanov/FITSxtractor',
+  download_url = 'https://github.com/ovanov/FITSxtractor/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['FITS', 'xml', 'csv', 'CLI programm'],
+  install_requires=[
+          'pandas',
+          'openpyxl',
+          'tqdm'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
   ],
 )
