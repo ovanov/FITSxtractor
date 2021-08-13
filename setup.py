9ff3f5b11426759f7e6af0f1ab6018a7a195f120
Jovanovic  Luka <ljovanovic@ethz.ch>
2021-08-13 14:14:01 +0200

    updated README.md

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'FITSxtractor',
   packages = find_packages(),
-  version = '0.9.1',
+  version = '0.9.2',
   license='MIT',
   description = 'This package extracts xml metadata from FITS output to a csv or xlsx file',
   long_description=long_description,
